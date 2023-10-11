72e53325b9b89701dc147fcf4b52e048ab6d9c47
Ravikiran <ravikiran150@gmail.com>
2023-10-11 10:44:09 +0530

    Create 01_variables.sh

---
+++ src/shell-scripting/01_variables.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+#Variables Variables in shell scripting are named storage locations that can hold data. They can be used to store any type of data, such as text, numbers, filenames, or even the output of other commands. Variables can be used to make shell scripts more efficient and reusable, as they allow you to avoid repeating yourself.
+#To create a variable in shell scripting, simply assign a value to it using the equal sign (=). for example
+
+name=Ravi
+role=Tester
+echo my name is $name and I am a $role
+
+#Output will "my name is Ravi and I am a Tester"
