956973ac0bc1f36feaeb2e3d3f90aeb297b8309e
Ravikiran <ravikiran150@gmail.com>
2023-10-11 13:06:50 +0530

    Create 04_user_input.sh

---
+++ src/shell-scripting/04_user_input.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#In shell scripting we can take the user input print the output. 
+echo "What is your name?"
+read name
+echo "Hi $name welcome"
+
+#We can write the command in another way
+echo "What is you name?"
+read -p "Enter your name " name #it will display the text enter your name for the user input
+echo hi $name welcome
+
+
+
+
